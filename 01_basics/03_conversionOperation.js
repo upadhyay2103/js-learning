292f791c9d9424af00830ba61d3f86c4dd92469f
upadhyay2103 <124630136+upadhyay2103@users.noreply.github.com>
2024-06-09 11:53:04 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score="33";
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber=Number(score)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // do keep in mind that score can be string which is converted into a number 
 // suppose score="33abc" then in that it will convert it to a number but when you wil  print it it gives Nan
@@ -17,3 +17,41 @@ console.log(typeof valueInNumber);
 
 // ""=> false
 // "aruanb" => true
+
+// ********************************* operations **************************************
+
+let val=3
+let NegVal=-val
+// console.log(NegVal);
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Aryan"
+let str3=str1+str2
+console.log(str3);
+
+// some imp edges cases
+
+// console.log("1"+2); => it gives 12
+// console.log(1+"2") => it gives 12
+// console.log("1"+2+2); => it gives 122
+// console.log(1+2+"2"); => it gives 32
+
+// console.log(+true); => it gives 1
+// console.log(+""); => it gives 0
+
+// prefix
+let a=4
+let b=++a
+// console.log(a); it will give 5 assignement after increment
+
+// postfix
+let d=4
+let c=d++
+// console.log(c); it will give 4 assignment before increment
