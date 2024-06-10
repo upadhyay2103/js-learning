ee644f11a5ff69501b5de39abed544b5b3c4d0a1
upadhyay2103 <124630136+upadhyay2103@users.noreply.github.com>
2024-06-10 19:13:28 +0000

    done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+// singleton objects -> when created thhrough constructor
+const myObj=new Object()
+const myObj2={}
+// plz note that above two objects have no difference at all
+
+myObj2.id="123abc"
+myObj2.name="aryan"
+myObj2.loggedIn=false
+
+// we can also have nesting inside objects 
+
+const obj={}
