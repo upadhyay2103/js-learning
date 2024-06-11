c1141c2cd8c820dfcd8116ce5d1b784581eee699
upadhyay2103 <124630136+upadhyay2103@users.noreply.github.com>
2024-06-11 12:10:15 +0000

    scope part 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,30 @@ console.log(a);
 // basically there are two scopes block scope and a global scope 
 // block scope is the scope of a block it may be consitional ,loops ,functions but not object
 // global scope is whatever outside the block scope 
+
+// now let us nesting of scopes
+
+function one(){
+    const user="Aryan"
+    function two(){
+        const website="youtube"
+        console.log(user);
+    }
+
+    // console.log(website);
+    two()
+}
+
+one()
+
+// above function gives error because website is block scope for function two but is accessed outside it if u remove log of website it will work fine
+
+// ++++++++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++++
+
+// functions can also be declared in an variable
+const power2=function(num){
+    return num**2;
+}
+
+console.log(power2(2))
+// one thing is that you can't call this function before defining it
