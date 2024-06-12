0e8f93c97d91c13d29d1296210803b1f60df8d02
upadhyay2103 <124630136+upadhyay2103@users.noreply.github.com>
2024-06-12 05:33:02 +0000

    while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,24 @@
+// while loop
+
+let index=0;
+while(index<=10){
+    console.log(`index is ${index}`);
+    index=index+1
+}
+
+// while loop over an array
+
+myarr=["flash","batman","superman"]
+let i=0;
+while(i<myarr.length){
+    console.log(myarr[i]);
+    i=i+1
+}
+
+// do while loop
+
+let int=0
+do {
+    console.log("hi");
+    int++
+} while (int<10);
