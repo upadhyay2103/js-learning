a4ec206cbf85c1fe44485e67c7f5a808785730cb
upadhyay2103 <124630136+upadhyay2103@users.noreply.github.com>
2024-06-26 18:36:11 +0000

    adv

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -39,3 +39,51 @@ const promiseThree=new Promise((resolve,reject)=>{
 promiseThree.then((user)=>{
     console.log(user);
 })
+
+const promiseFour=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true;
+        if(!error){
+            resolve({username:"Aryan", password:"123"})
+        }else
+        {
+            reject(`ERROR : something went wrong`);
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+    console.log(user.username);
+    return user.username;
+}).then((username)=>{
+    console.log(username);
+}).catch((error)=>{
+    console.log(error);
+}).finally(()=>{
+    console.log("hello bhaiya kya haal chaal");
+})
+
+const promiseFive=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true;
+        if(!error){
+            resolve({username:"javascript", password:"1234"})
+        }else
+        {
+            reject(`ERROR : something went wrong`);
+        }
+    },1000)
+})
+
+// neeche ddiye gae eg me async await use hua haui yeh bhi ek alternate m ethod hai but dhyaan rakhna try catch block me use karna iska
+
+async function promiseFiveConsume(){
+    try {
+        const response=await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+promiseFiveConsume()
